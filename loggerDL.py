3d3b558992c643171aaae95a3e1a023c1778608f
qJay44 <m70155302@yandex.ru>
2022-05-30 21:46:02 +0300

    video handler

--- loggerDL.py
+++ loggerDL.py
@@ -1,9 +1,9 @@
-class MyLogger:
+class Logger:
     __msg_info = f'[download]   0.0% of 0.0MiB at 0.0KiB/s ETA 00:00'
     __bot = None
     __botMessage = None
 
-    def __init__(self, bot, botMessage) -> None:
+    def __init__(self, bot, botMessage):
         self.__bot = bot
         self.__botMessage = botMessage
 
@@ -24,7 +24,7 @@ class MyLogger:
     def error(self, msg):
         pass
 
-    def myHook(self, d):
+    def downloadHook(self, d):
         if d['status'] != 'error':
             self.__bot.edit_message_text(
                 self.__msg_info,
--- tgBot2.py
+++ tgBot2.py
@@ -2,12 +2,11 @@ import telebot
 import logging
 import json
 import yt_dlp
+import os
 from config import API_KEY
-from telebot import custom_filters
-from telebot import types
+from telebot import custom_filters, types
 from shutil import rmtree
-from os import mkdir
-from Logger import MyLogger
+from loggerDL import Logger
 
 bot = telebot.TeleBot(API_KEY)
 bot.add_custom_filter(custom_filters.TextStartsFilter())
@@ -36,8 +35,8 @@ def get_text_messages(message):
         try:
             with yt_dlp.YoutubeDL({'simulate': True}) as ydl:
                 ydl.download(userLink)
-        except Exception as e:
-            return bot.send_message(message.chat.id, str(e).split(': ')[1])
+        except Exception:
+            return bot.send_message(message.chat.id, 'Wrong link...')
 
         # Создание кнопок типа "inline"
         markup_inline = types.InlineKeyboardMarkup()
@@ -51,66 +50,82 @@ def get_text_messages(message):
 @bot.callback_query_handler(func=lambda call: True)
 def call_answer(call):
     global userLink, link_enabled
+    link_enabled = False
+
+    # Строка ниже сообщает телеграму, что кнопка была обработана (иначе будет вечное ожидание)
+    bot.answer_callback_query(callback_query_id=call.id)
+    msg = bot.send_message(call.message.chat.id, 'Скачиваю...')
+    download_logger = Logger(bot, msg)
+    os.mkdir('./temp')
+
+    # creating temp folder and download audiostream there
+    temp_path = './temp/%(uploader)s - %(fulltitle)s.%(ext)s'
+
+    # yt-dlp options
+    ydl_opts = {
+        'writeinfojson'      : True,
+        'writethumbnail'     : True,
+        'format'             : 'mp3/ba' if call.data == 'audio' else 'mp4/bv',
+        'logger'             : download_logger,
+        'progress_hooks'     : [download_logger.downloadHook],
+        'outtmpl': { 
+            'infojson' : './temp/url.%(ext)s',
+            'thumbnail': temp_path,
+            'default'  : temp_path
+        },
+        'postprocessors': [{
+            'key': 'FFmpegThumbnailsConvertor',
+            'format': 'jpg'
+        }, {
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3'
+        } if call.data == 'audio' else {
+            'key': 'FFmpegVideoConvertor',
+            'preferedformat': 'mp4'         # "preferred" ?
+        }]
+    }
+
+    # download file
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download(userLink)
+
+    # open metadata file
+    with open('temp/url.info.json', encoding='utf-8') as f:
+        data = json.load(f)
+        if call.data == 'video':
+            video_width  = data['width']
+            video_height = data['height']
+        video_id     = data['id']
+        title        = data['title']
+
+    # get path for an audio file and a thumbnail
+    filename = next(os.walk('temp/'), (None, None, []))[2]
+    filename.remove('url.info.json')
+    filename = os.path.splitext(filename[0])[0]
+    temp_file = f'./temp/{filename}{".mp3" if call.data == "audio" else ".mp4"}'
+    thumbnail = f'./temp/{filename}.jpg'
+    caption = f"""
+        <a href='https://song.link/y/{video_id}'>
+            <i>song.link</i>
+        </a>"""
+
+    # Отправляет трек
+    bot.send_message(call.message.chat.id, 'Выгружаю...')
+
     if call.data == 'audio':
-        # Строка ниже сообщает телеграму, что кнопка была обработана (иначе будет вечное ожидание)
-        bot.answer_callback_query(callback_query_id=call.id)
-        msg = bot.send_message(call.message.chat.id, 'Скачиваю аудиотрек...')
-        download_logger = MyLogger(bot, msg)
-
-        # creating temp folder and downloading audiostream there
-        mkdir('./temp')
-        audio_path = './temp/%(uploader)s - %(fulltitle)s.%(ext)s'
-
-        ydl_opts = {
-            'writeinfojson'      : True,
-            'writethumbnail'     : True,
-            'format'             : 'mp3/ba',
-            'logger'             : download_logger,
-            'progress_hooks'     : [download_logger.myHook],
-            'outtmpl': { 
-                'infojson' : './temp/url.%(ext)s',
-                'thumbnail': audio_path,
-                'default'  : audio_path
-            },
-            'postprocessors': [{
-                'key': 'FFmpegThumbnailsConvertor',
-                'format': 'jpg'
-            }, {
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3'
-            }]
-        }
-
-        # download file
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download(userLink)
-
-        # open metadata file
-        with open('temp/url.info.json') as f:
-            data = json.load(f)
-            video_id = data['id']
-            title = data['title']
-            uploader = data['uploader']
-
-        # setting path for an audio file and a thumbnail
-        audio_file = audio_path % {'uploader': uploader, 'fulltitle': title, 'ext': "mp3"}
-        thumbnail  = audio_path % {'uploader': uploader, 'fulltitle': title, 'ext': "jpg"}
-        caption = f"""
-            <a href='https://song.link/y/{video_id}'>
-                <i>song.link</i>
-            </a>"""
-
-        # Отправляет трек
-        bot.send_message(call.message.chat.id, 'Выгружаю трек...')
-        bot.send_audio(call.message.chat.id, open(audio_file, 'rb'), 
+        bot.send_audio(call.message.chat.id, open(temp_file, 'rb'), 
                        thumb=open(thumbnail, 'rb'),
                        title=title, 
                        caption=caption, 
                        parse_mode='html')
+    else:
+        bot.send_video(call.message.chat.id, open(temp_file, 'rb'),
+                       thumb=open(thumbnail, 'rb'),
+                       width=video_width,
+                       height=video_height, 
+                       caption=title)
 
-        rmtree('./temp')
-
-    link_enabled = False
+    rmtree('./temp')
         
     
-bot.polling()
+bot.polling(none_stop=True, timeout=123)
